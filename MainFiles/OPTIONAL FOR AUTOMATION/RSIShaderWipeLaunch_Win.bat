84d153ee8d1c14fd1f81d10f15d668334afab0e3
DavidRoseLincs <davidroselincs@gmail.com>
2023-03-14 12:41:15 +0000

    Update to the automatic bats

--- MainFiles/OPTIONAL FOR AUTOMATION/RSIShaderWipeLaunch_Win.bat
+++ MainFiles/OPTIONAL FOR AUTOMATION/RSIShaderWipeLaunch_Win.bat
@@ -1,47 +1,69 @@
-title RSIShaderWipeLaunch
-RD /S /Q "E:\StarCitizen\Roberts Space Industries\StarCitizen\LIVE\USER\Client\0\shaders"
-:: DOUBLE CHECK THE LOCATIONS OF THE FILES
+@echo off
+title RSI Launcher Editor - Automatic Edition - Shader Wipe Boot
 
-:: This script will run the python script that will run the program
-:: It will also check if python is installed and if it is in PATH
-:: If it is not, it will tell you to install it and put it in PATH
-:: If you already have it installed, you might have forgotten to put it in PATH
+echo.
+echo Checking for Python:
+py -3 --version
+echo.
+echo Checking for Node.js:
+where node
 
-where python
+echo.
+echo.
+echo This script will run a check for Python and Node.js, install them if they are not installed, and then run the script.
+echo THIS IS NOT AUTOMATIC and is only a single run. If you wish to automate it whenever you run the launcher, please look 
+echo at the github readme again and look into the optional choices RSI_Launcher_Win.bat and RSIShaderWipeLaunch_Win.bat.
+echo.
+echo.
+
+py -3 --version
 IF %ERRORLEVEL% NEQ 0 (
   cls
-  echo You might need to install python first!
-  echo https://www.python.org/downloads/
+  echo Python is not installed or not in PATH! Winget can install it for you and is about to run if you put Y if N it 
+  echo will cancel and continue to run the script and fail
   echo.
-  echo If you do already have it installed, you might have forgotten to put it in PATH
-  echo Reinstall it and make sure to check the box that says "Add Python to PATH"
-  echo.
+  CHOICE /C YNC /M "Press Y for Yes, N for No ."
+ 
 
-  :: Pause and Exit is used to make sure that you are able to
-  :: read the message before the window closes
+  :: Pause and Exit is used to make sure that you are able to read the message before the window closes
+  :: prompts the user to confirm that they want winget to install python 3.12.0a1
+  
+
+  winget install -e --id=Python.Python.3.10 -v "3.10.2" --scope=machine
+  echo might need to restart the machine for the commands to be recongnized
+  echo.
+  echo Python is being downloaded from here pleaes use this version or higher
+  echo https://www.python.org/downloads/release/python-3102/
+  echo.
+  echo please make sure to check the box that says "Add Python to PATH" and PIP
   pause
-  exit
 )
 
 where node
 IF %ERRORLEVEL% NEQ 0 (
   cls
-  echo You might need to install Node.js first!
-  echo https://nodejs.org/en/download/
+  echo Node.js is not installed! Winget can install it for you and is about to run if you put Y if N it will cancel 
+  echo and continue to run the script and fail
   echo.
-  echo If you do already have it installed, you might need to update it
-  echo.
+  CHOICE /C YNC /M "Press Y for Yes, N for No."
 
   :: Pause and Exit is used to make sure that you are able to
   :: read the message before the window closes
+
+  winget install --id=OpenJS.NodeJS -e 
+  echo Node.js is being downloaded from here 
+  echo https://nodejs.org/en/download/
+  echo.
   pause
-  exit
 )
 
 "C:\Python311\python.exe" "E:\StarCitizen\rotate_screenshot_splash.py"
 :: DOUBLE CHECK THE LOCATIONS OF THE FILES
 
 "C:\Python311\python.exe" "E:\StarCitizen\edit_launcher_carousel.py"
+:: DOUBLE CHECK THE LOCATIONS OF THE FILES
+
+RD /S /Q "E:\StarCitizen\Roberts Space Industries\StarCitizen\LIVE\USER\Client\0\shaders"
 :: DOUBLE CHECK THE LOCATIONS OF THE FILES
 
 Start ""  "E:\StarCitizen\RSI Launcher\RSI Launcher.exe"
